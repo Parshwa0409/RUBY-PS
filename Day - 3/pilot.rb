23d6356e4b04c6adb21c133b3c021d55a5dce3d1
Parshwa0409 <parshwapail9@gmail.com>
2024-03-04 17:02:45 +0530

    Added Bank Deposit

--- Day - 3/pilot.rb
+++ Day - 3/pilot.rb
@@ -3,6 +3,7 @@ module CommonOperations
     def withdrwal_possible?(amt,usr)
         amt <= usr[:balance]
     end
+
     def withdrawl(usr)
         begin
             print("Enter the amount to be withdrwan: ")
@@ -18,11 +19,11 @@ module CommonOperations
             puts("ERR: #{e}")
         end
     end
+
     def check_balance(usr)
         puts("Account Balance: #{usr[:balance]}")
     end
 end
-
 
 
 class ATM
@@ -54,7 +55,7 @@ class ATM
 
     def menu
         
-        print("\nChoose the operaion to be done:\n1)Withdwarl\n2)Check Balance\n3)Quit\nEnter Your Choice: ")
+        print("\nChoose the operaion to be done:\n1)Withdrawl\n2)Check Balance\n3)Quit\nEnter Your Choice: ")
         choice = gets().chomp().to_i()
 
         puts()
@@ -75,4 +76,71 @@ class ATM
 end
 
 
-ATM.new({name:"Paras", balance:1008, pin:1008})
+class Bank
+    include CommonOperations
+    
+    attr_accessor :usr, :block_user
+
+    def initialize(usr)
+        @usr = usr
+        @block_user = false
+
+        main()
+    end
+
+    def main
+        verify
+
+        if(!block_user)
+            menu
+        end
+    end
+
+    def verify
+        puts("\nHello #{@usr[:name]},")
+        print("Enter your Pin: ")
+        pin = gets().chomp().to_i()
+        if pin != @usr[:pin]
+            @block_user = true
+        end
+    end
+
+    def deposit
+        begin
+            print("Enter the amount to be deposied: ")
+            amt = gets().chomp().to_i()
+
+            usr[:balance] += amt
+        rescue => e
+            puts("ERR: #{e}")
+        end
+    end
+
+    def menu
+        
+        print("\nChoose the operaion to be done:\n1)Deposit\n2)Check Balance\n3)Quit\nEnter Your Choice: ")
+        choice = gets().chomp().to_i()
+
+        puts()
+        case choice
+        when 1
+            self.deposit
+        when 2
+            self.check_balance(@usr)
+        when 3
+            puts("Bye Bye!")
+            return
+        else
+            puts("Invalid option, Please try again!")
+        end
+
+        menu()
+    end 
+end
+
+
+user_1 = {name:"Paras", balance:1008, pin:1008}
+
+Bank.new(user_1)
+
+ATM.new(user_1)
